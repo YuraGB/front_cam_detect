5922df7cdefde6e856f6693fe96bbe3155f56905
Yurii Hurianov <YuraGB>
2026-04-06 15:03:48 +0300

    fix start

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,6 +4,6 @@ const STREAM_URLS =  ['ws://localhost:3002/ws?type=liveStream', 'ws://localhost:
 export type StreamType = typeof STREAM_TYPES[number];
 export type StreamURL = typeof STREAM_URLS[number];
 
-export type VideoResources = Record<string, Record<string, HTMLCanvasElement>>;
+export type VideoResources = Partial<Record<string, Partial<Record<StreamType, HTMLCanvasElement>>>>;
 
 export { STREAM_TYPES, STREAM_URLS}
