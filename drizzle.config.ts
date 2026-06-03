aa41fb8c8e0de7045f8c318919310d24b17d5ec2
YuraGB <yuhur1985@gmail.com>
2026-06-03 14:36:54 +0300

    fix: removed consoles

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'drizzle-kit'
 
 export default defineConfig({
   dialect: 'sqlite',
-  schema: './src/db/schema/auth.ts',
+  schema: './src/server/db/schema/auth.ts',
   out: './drizzle',
   dbCredentials: {
     url: 'file:./auth.sqlite',
