7ec2c788025206a1999c876c94eb28d37b97bf01
YuraGB <yuhur1985@gmail.com>
2026-06-25 10:55:38 +0300

    fix: reconnect webRTc connection git status

--- src/lib/asyncActionHandler/index.ts
+++ src/lib/asyncActionHandler/index.ts
@@ -1,3 +1,5 @@
+import { logger } from '../frontend_logger'
+
 type Result<T, TError = unknown> =
   | { data: T; error: null }
   | { data: null; error: TError }
@@ -9,6 +11,7 @@ export async function tryCatch<T>(fn: () => Promise<T>): Promise<Result<T>> {
       error: null,
     }
   } catch (error) {
+    logger.error('try catch has an error')
     return {
       data: null,
       error,
@@ -20,6 +23,16 @@ export function safeJsonParse<T = unknown>(value: string): T | null {
   try {
     return JSON.parse(value)
   } catch {
+    logger.error('JSON parse failed', value)
     return null
+  }
+}
+
+export function safeJsonStringify<T = unknown>(value: T): string {
+  try {
+    return JSON.stringify(value)
+  } catch {
+    logger.error('Json stringify failed', value)
+    return ''
   }
 }
